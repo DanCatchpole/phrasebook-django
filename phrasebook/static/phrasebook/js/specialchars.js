95dce388197e33aee4f6a8f10726b04d74d5f573
Daniel <dan@dcatcher.me>
2017-04-27 18:47:42 +0100

    3 a's make a å

--- phrasebook/static/phrasebook/js/specialchars.js
+++ phrasebook/static/phrasebook/js/specialchars.js
@@ -1,11 +1,11 @@
 $("body").on('keyup', 'input[type=text]', function(e) {
     let $input = $(this);
     let inVal = $input.val();
-    if (inVal.endsWith("aa")) {
-        $input.val(inVal.substr(0, inVal.length - 2) + "å");
+    if (inVal.endsWith("aaa")) {
+        $input.val(inVal.substr(0, inVal.length - 3) + "å");
     }
 
-    if (inVal.endsWith("AA")) {
-        $input.val(inVal.substr(0, inVal.length - 2) + "Å");
+    if (inVal.endsWith("AAA")) {
+        $input.val(inVal.substr(0, inVal.length - 3) + "Å");
     }
 })
