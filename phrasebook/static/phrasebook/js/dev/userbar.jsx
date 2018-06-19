be023e1374c4a85c1f58e206591f45aabcd6f83a
DCatcher <dan@dcatcher.me>
2018-06-19 22:24:09 +0100

    Tweaks and apparent line ending change

--- phrasebook/static/phrasebook/js/dev/userbar.jsx
+++ phrasebook/static/phrasebook/js/dev/userbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {render} from 'react-dom';
 
 class UserBar extends React.Component {
 
@@ -15,11 +14,11 @@ class UserBar extends React.Component {
 
         return (
             <a className="user-bar">
-                <img className="user-icon" src="https://dcatcher.me/assets/dcatcher.jpg" alt="userIcon"/>
+                <img className="user-icon" src="https://dcatcher.me/assets/userIcon.svg" alt="userIcon"/>
                 <img className="flag" src={"https://dcatcher.me/assets/" + this.props.details.current_language + ".svg"} alt=""/>
                 <div className="progress-bar">
                     <span>Level {this.props.details.level}</span>
-                    <div className="actual-progress"></div>
+                    <div className="actual-progress" style={{width: this.props.details.xp_percentage + "%"}}></div>
                 </div>
             </a>
         );
---
+++ phrasebook/static/phrasebook/js/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Daniel on 12/12/2017.
+ */
